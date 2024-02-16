6268e4334d343079bdc152790629d1635596bc38
andres nahuel antola <aantola@dc.uba.ar>
2024-02-16 13:58:47 -0500

    fixed(login): login and logout from backend

--- src/context/accountContext/index.tsx
+++ src/context/accountContext/index.tsx
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 import  React, { createContext, useState } from 'react';
 
+
+
 interface AccountProviderProps {
 	children: React.ReactNode;
 }
@@ -8,22 +10,36 @@ interface AccountProviderProps {
 interface accountContextType {
     tokenExists: boolean
     login: (t: string) => void
-    logout: () => void
+    logout: () => void,
+    userData: null | user,
 }
+
+
+interface user {
+    username: string,
+    id: number,
+    public: boolean,
+    googleid: string,
+}
+
 
 const AccountContext = createContext<accountContextType>({
     tokenExists: false,
     login: () => {},
     logout: () => {},
+    userData: null,
 });
 
 
 const AccountProvider : React.FC<AccountProviderProps> = ({children}) => {
     
     const [tokenExists,setTokenExists] = useState(localStorage.getItem('token') != null) 
+    const [userData, setUserData] = useState(localStorage.getItem('userData') ? JSON.parse(localStorage.getItem('userData') || '')[0] : null);
+
+    console.log("userData",userData);
 
     const login = async (token : string) => {
-        // Login code
+        // Request for verification backend
         await fetch(process.env.REACT_APP_BACKEND_URL+ "user/login",{
             method: 'POST',
             headers: {
@@ -33,8 +49,16 @@ const AccountProvider : React.FC<AccountProviderProps> = ({children}) => {
             body: JSON.stringify({token})
         }).then((res : Response) => {
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
-            res.json().then((data : any) => {
-                console.log(data)
+            res.json().then((data : [user]) => {
+
+                setTokenExists(true);
+                setUserData(data[0]);
+
+                localStorage.setItem('userData', JSON.stringify(data));
+                // TODO: move token get to backend and use it as cookie with http-only
+                // using localstorage to save token is not the best option, but for now it will do
+                // there isn't any sensible user info
+                localStorage.setItem('token', token);
             })  
 
         
@@ -43,12 +67,13 @@ const AccountProvider : React.FC<AccountProviderProps> = ({children}) => {
             console.error("Authentification error: " + e.message)
         })
         
-        //setTokenExists(true)
-        //localStorage.setItem('token', token);
+        
     }
 
     const logout = () => {
-        setTokenExists(false)
+        setTokenExists(false);
+        setUserData(null);
+        localStorage.removeItem('userData')
         localStorage.removeItem('token');
     } 
 
@@ -56,6 +81,7 @@ const AccountProvider : React.FC<AccountProviderProps> = ({children}) => {
         login,
         logout,
         tokenExists,
+        userData,
     }
 
 
